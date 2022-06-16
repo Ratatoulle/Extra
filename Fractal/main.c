3b0797e066014373068b59f07f93ca69639606d7
Ratatoulle <1vasilev.andrey@gmail.com>
2022-06-16 14:41:26 +0500

    refactoring

--- Fractal/main.c
+++ Fractal/main.c
@@ -5,8 +5,9 @@
 #define HEIGHT 1020
 #define LINELENGTH 500
 #define SYMORDER 10
-int SymIteration = 0;
 #define COMPRESSION 1.8
+
+int symIteration = 0;
 
 typedef struct Line
 {
@@ -15,80 +16,74 @@ typedef struct Line
 	float length;
 }line;
 
-void drawTree(line Branch, int SymOrder, int Dir);
-void rotatedBranch(line Branch, int SymOrder, int Dir, float Offset, float x, float y);
-void initTree(line* Tree);
-void rectangularBranches(line Branch, int SymOrder, int Dir, float Offset, float x, float y);
+void drawTree(line, int, int);
+void rotateBranch(line, int, int, float, float, float);
+void initTree(line*);
+void rectangularBranches(line, int, int, float, float, float);
 
 int main()
 {
-	line Tree;
-	InitWindow(WIDTH, HEIGHT, "Fractal's wood");
+	line tree;
+	InitWindow(WIDTH, HEIGHT, "Fractal wood");
 	static int FPS = 10;
 	while (!WindowShouldClose())
 	{
 		SetTargetFPS(FPS);
-		if (SymIteration > SYMORDER)
-			SymIteration = 0;
-		++SymIteration;
+		if (symIteration > SYMORDER)
+			symIteration = 0;
+		++symIteration;
 		BeginDrawing();
-		if (IsKeyDown(KEY_UP) && FPS < 60)
-			FPS++;
-		else if (IsKeyDown(KEY_DOWN) && FPS > 5)
-			FPS--;
-		ClearBackground(WHITE);
-		initTree(&Tree);
-		drawTree(Tree, 1, 1);
-		DrawText(TextFormat("FPS: %i", FPS), 10, 10, 25, BLACK);
-		DrawText("Press Up/Down to change rendering speed", 20, 30, 25, BLACK);
+		ClearBackground(GREEN);
+		initTree(&tree);
+		drawTree(tree, 1, 1);
 		EndDrawing();
 	}
 }
-void rotatedBranch(line Branch, int SymOrder, int Dir, float Offset, float x, float y)
+void rotateBranch(line branch, int symOrder, int dir, float offset, float x, float y)
 {
-	DrawLineEx(Branch.start, Branch.end, 3.0f, BLACK);
-	Branch.length /= COMPRESSION;
-	Offset = Branch.length / sqrt(2);
-	x += Offset;
-	y -= Offset;
-	Branch.start.x = Branch.end.x;
-	Branch.start.y = Branch.end.y;
-	Branch.end.x = x;
-	Branch.end.y = y;
-	drawTree(Branch, SymOrder + 1, 1);
-	x -= 2 * Offset;
-	Branch.end.x = x;
-	drawTree(Branch, SymOrder + 1, -1);
+	DrawLineEx(branch.start, branch.end, 3.0f, BLACK);
+	branch.length /= COMPRESSION;
+	offset = branch.length / sqrt(2);
+	x += offset;
+	y -= offset;
+	branch.start.x = branch.end.x;
+	branch.start.y = branch.end.y;
+	branch.end.x = x;
+	branch.end.y = y;
+	drawTree(branch, symOrder + 1, 1);
+	x -= 2 * offset;
+	branch.end.x = x;
+	drawTree(branch, symOrder + 1, -1);
 }
-void initTree(line* Tree)
+void initTree(line* tree)
 {
-	Tree->start.x = (float)WIDTH / 2;
-	Tree->start.y = (float)HEIGHT;
-	Tree->end.y = (float)HEIGHT - LINELENGTH;
-	Tree->end.x = (float)WIDTH / 2;
-	Tree->length = LINELENGTH;
+	tree->start.x = (float)WIDTH / 2;
+	tree->start.y = (float)HEIGHT;
+	tree->end.y = (float)HEIGHT - LINELENGTH;
+	tree->end.x = (float)WIDTH / 2;
+	tree->length = LINELENGTH;
 }
-void drawTree(line Branch, int SymOrder, int Dir)
+void drawTree(line branch, int symOrder, int Dir)
 {
 	float Offset = 0;
-	float x = Branch.end.x;
-	float y = Branch.end.y;
-	if (SymOrder == SymIteration)
+	float x = branch.end.x;
+	float y = branch.end.y;
+	if (symOrder == symIteration)
 		return;
-	else if (SymOrder % 2)
-		rotatedBranch(Branch, SymOrder, Dir, Offset, x, y);
-	else if (SymOrder % 2 == 0 && SymOrder != 0)
-		rectangularBranches(Branch, SymOrder, Dir, Offset, x, y);
+	else if (symOrder % 2)
+		rotateBranch(branch, symOrder, Dir, Offset, x, y);
+	else if (symOrder % 2 == 0 && symOrder != 0)
+		rectangularBranches(branch, symOrder, Dir, Offset, x, y);
 }
-void rectangularBranches(line Branch, int SymOrder, int Dir, float Offset, float x, float y)
+void rectangularBranches(line branch, int symOrder, int dir, float offset, float x, float y)
 {
-	DrawLineEx(Branch.start, Branch.end, 3.0f, BLACK);
-	Branch.length /= COMPRESSION;
-	Branch.start.x = Branch.end.x;
-	Branch.start.y = Branch.end.y;
-	Branch.end.x += Dir * Branch.length;
-	drawTree(Branch, SymOrder + 1, 1);
-	Branch.end.x -= Dir * Branch.length;
-	Branch.end.y -= Branch.length;
-	drawTree(Branch, SymOrder + 1, 1);
+	DrawLineEx(branch.start, branch.end, 3.0f, BLACK);
+	branch.length /= COMPRESSION;
+	branch.start.x = branch.end.x;
+	branch.start.y = branch.end.y;
+	branch.end.x += dir * branch.length;
+	drawTree(branch, symOrder + 1, 1);
+	branch.end.x -= dir * branch.length;
+	branch.end.y -= branch.length;
+	drawTree(branch, symOrder + 1, 1);
 }
